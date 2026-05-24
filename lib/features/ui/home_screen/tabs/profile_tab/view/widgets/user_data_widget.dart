cdb7b53051888a1123f5bead4c24b7ccd9dd07ce
abdallah elnshar <abdallah.elnshar.123@gmail.com>
2026-05-24 16:50:36 +0300

    fix : logout bug

--- lib/features/ui/home_screen/tabs/profile_tab/view/widgets/user_data_widget.dart
+++ lib/features/ui/home_screen/tabs/profile_tab/view/widgets/user_data_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies/core/constants/app_constants.dart';
 import 'package:movies/core/utils/app_assets.dart';
 import 'package:movies/domain/entities/response/user/my_user.dart';
+import 'package:movies/features/ui/auth/auth_state.dart';
 
 import '../../../../../../../core/utils/app_colors.dart';
 import '../../../../../../../core/utils/app_styles.dart';
@@ -14,34 +15,42 @@ class UserDataWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final MyUser? currentUser = context.watch<AuthCubit>().currentUser;
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      spacing: context.height * 0.015,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          width: context.width * 0.25,
-          height: context.width * 0.25,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: AppColors.darkGrayColor,
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage(
-                currentUser?.avatarIndex != -1
-                    ? AppConstants.avatarList[currentUser?.avatarIndex ?? -1]
-                    : AppAssets.fallbackUserImage,
+    return BlocBuilder<AuthCubit, AuthState>(
+      builder: (BuildContext context, AuthState state) {
+        final MyUser currentUser = context.read<AuthCubit>().currentUser!;
+
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: context.height * 0.015,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: context.width * 0.25,
+              height: context.width * 0.25,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: AppColors.darkGrayColor,
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                    currentUser.avatarIndex != -1
+                        ? AppConstants.avatarList[currentUser.avatarIndex]
+                        : AppAssets.fallbackUserImage,
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-        Text(
-          currentUser?.name ?? '',
-          style: AppStyles.robotoBold20White(context),
-          textAlign: TextAlign.center,
-        ),
-      ],
+            Text(
+              currentUser.name,
+              style: AppStyles.robotoBold20White(context),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        );
+      },
+      buildWhen: (previous, current) =>
+          current is AuthAuthenticated ||
+          current is AccountDetailsUpdateSuccess,
     );
   }
 }
